a9c1d7dba099369def656e33badfff444cce7790
Hao <orctom@gmail.com>
2016-01-01 23:46:35 +0800

    delete not working

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import urllib2, httplib
+import urllib2
 import json
 import math
+import re
+from datetime import datetime
 from os import environ
-from flask import Flask, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
 
 REGISTRY_URL = environ.get("REGISTRY_URL", "http://localhost:5000")
 DEBUG = environ.get("DEBUG", True)
@@ -16,55 +18,101 @@ app.jinja_env.globals['registry'] = REGISTRY_URL
 @app.route("/")
 def index():
     result = _query("/_catalog")
-    return render_template('index.html', repos=result['repositories'])
+    repos = []
+    image = request.args.get('image')
+    print image
+    for repo in result['repositories']:
+        if None != image and image not in repo:
+            continue
+        tags = _query("/%s/tags/list" % repo)
+        repos.append({"name": repo, "tags": len(tags['tags'])})
+    return render_template('index.html', repos=repos)
 
 @app.route("/images/<repo>")
 @app.route("/images/<repo>/<image>")
 def images(repo, image=None):
-    if DEBUG:
-        print "repo:", repo, ", image:", image
     if None == image:
         result = _query("/%s/tags/list" % repo)
     else:
         result = _query("/%s/%s/tags/list" % (repo, image))
-    if DEBUG:
-        print result
-    return render_template('images.html', name=result['name'], tags=result['tags'])
 
+    tags = []
+    for tag in result["tags"]:
+        manifests = _manifests(repo, tag, image)
+        tags.append({
+            "tag": tag,
+            "tagId": manifests["tagId"],
+            "created": manifests["created"],
+            "layersCount": manifests["layersCount"],
+            "size": manifests["size"],
+        })
+    info = request.args.get('info')
+    error = request.args.get('error')
+    return render_template('images.html',
+                           name=result['name'],
+                           tags=tags,
+                           info=info,
+                           error=error)
 
 @app.route("/delete/<repo>/<tag>")
 @app.route("/delete/<repo>/<image>/<tag>")
 def delete(repo, tag, image=None):
-    conn = httplib.HTTPConnection("localhost:5000")
     if None == image:
         result = _query("/%s/manifests/%s" % (repo, tag))
         digest = result["digest"]
-        conn.request("DELETE", "/v2/%s/manifests/%s" % (repo, digest))
+        url = REGISTRY_URL + "/v2/%s/manifests/%s" % (repo, digest)
     else:
         result = _query("/%s/%s/manifests/%s" % (repo, image, tag))
         digest = result["digest"]
-        conn.request("DELETE", "/v2/%s/%s/manifests/%s" % (repo, image, digest))
+        url = REGISTRY_URL + "/v2/%s/%s/manifests/%s" % (repo, image, digest)
 
-    response = conn.getresponse()
-    content = response.read()
-    print content
-    return redirect("/images/" + repo)
+    try:
+        req = urllib2.Request(url)
+        req.get_method = lambda: 'DELETE'
+        res = urllib2.urlopen(req)
+
+        content = json.loads(res.read())
+        print content
+        image_full_name = (repo + ":" + tag) if Node == image else (repo + "/" +image + ":" + tag)
+        info = "Image: " + image_full_name + " deleted."
+        error = None
+    except urllib2.HTTPError:
+        error = "Operation not supported yet by registry."
+        info = None
+
+    return redirect(url_for("images",
+                            repo=repo,
+                            image=image,
+                            info=info,
+                            error=error))
 
 @app.route("/manifests/<repo>/<tag>")
 @app.route("/manifests/<repo>/<image>/<tag>")
 def manifests(repo, tag, image=None):
+    result = _manifests(repo, tag, image)
+    return render_template('manifests.html',
+                           name=result['name'],
+                           tag=result['tag'],
+                           tagId=result["tagId"],
+                           digest=result["digest"],
+                           totalSize=result["size"],
+                           layers=result["layers"])
+
+def _manifests(repo, tag, image=None):
     if None == image:
         result = _query("/%s/manifests/%s" % (repo, tag))
     else:
         result = _query("/%s/%s/manifests/%s" % (repo, image, tag))
 
     layers = []
-    totalSize = 0
+    total_size = 0
     for history in result['history']:
-        data = json.loads(history['v1Compatibility'])
+        raw_data = history['v1Compatibility'].replace('\u0026\u0026', '\u0026\u0026 \\\\<br/>')
+        raw_data = re.sub('\s{8}', ' \\\\\\\\<br/>', raw_data)
+        data = json.loads(raw_data)
         if 'Size' in data:
             size = int(data['Size'])
-            totalSize += size
+            total_size += size
         else:
             size = 0
         layers.append({
@@ -73,27 +121,50 @@ def manifests(repo, tag, image=None):
             'command': data['container_config']['Cmd'][2],
             'size': _filesizeformat(size)})
     layers.sort(key=_getCreated)
-    id=layers[0]["id"]
-    return render_template('manifests.html',
-                           name=result['name'],
-                           tag=result['tag'],
-                           id=id,
-                           digest=result["digest"],
-                           totalSize=_filesizeformat(totalSize),
-                           layers=layers)
+    image_id = layers[-1]["id"]
+    created = layers[-1]["created"][0:19]
+    image_created = datetime.strptime(created, "%Y-%m-%dT%H:%M:%S").strftime("%Y %b %d %H:%M:%S")
+
+    return {
+        "name": result["name"],
+        "tag": tag,
+        "tagId": image_id,
+        "created": image_created,
+        "size": _filesizeformat(total_size),
+        "digest": result["digest"],
+        "layers": layers,
+        "layersCount": len(layers)
+    }
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html',
+                           title="Page Not Found",
+                           message="Maybe an UFO took it away..."), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    if DEBUG:
+        print e
+    return render_template('error.html',
+                           title="Error",
+                           message="Something went wrong, and the engineer is napping..."), 500
+
+@app.errorhandler(urllib2.URLError)
+def url_error(e):
+    if DEBUG:
+        print e
+    return render_template('error.html',
+                           title="Registry Not Available",
+                           message="The registry is not available: " + REGISTRY_URL), 503
 
 def _query(path):
     if DEBUG:
         print "querying:", REGISTRY_URL + path
-    try:
-        response = urllib2.urlopen(REGISTRY_URL + "/v2" + path)
-        result = json.loads(response.read())
-        if "Docker-Content-Digest" in response.headers:
-            result["digest"] = response.headers["Docker-Content-Digest"]
-    except Exception as e:
-        if DEBUG:
-            print e, "Path:", path
-        result = ''
+    response = urllib2.urlopen(REGISTRY_URL + "/v2" + path)
+    result = json.loads(response.read())
+    if "Docker-Content-Digest" in response.headers:
+        result["digest"] = response.headers["Docker-Content-Digest"]
     return result
 
 def _getCreated(data):
